a9e77c7fe9a7ed7ec71f8beb97af3f4024a13c7f
eruj22 <80893722+eruj22@users.noreply.github.com>
2021-05-31 20:52:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // set date to countdown to in ms
-let d = '2021, 12, 31';
+let d = '2022, 1, 1';
 let start = new Date(d).getTime();
 // time in seconds from target time till now
 let secondsToDate = (start - Date.now()) / 1000;
